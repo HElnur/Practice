98202152c1754671ed766480c48053d8cb253e20
Your Name <you@example.com>
2021-10-25 23:58:33 +0400

    Practice assignment

---
+++ Practice/Practice/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Assignment-1
+            //char[] digits = { '1', '2', '3', '4', '5', '6', '7', '7', '8', '9', '0' };
+            //bool hasChar = false;
+
+            //Console.WriteLine("Yazi daxil edin:");
+            //string enterString = Console.ReadLine();
+
+            //for(int i = 0; i < enterString.Length; i++)
+            //{
+            //    var letter = enterString[i];
+            //    for(int j = 0; j < digits.Length; j++)
+            //    {
+            //        if (letter == digits[j])
+            //        {
+            //            hasChar = true;
+            //            break;
+            //        }
+
+
+            //        else
+            //            hasChar = false;
+
+            //    }
+            //}
+
+            //if (hasChar == false)
+            //    Console.WriteLine(enterString + " stringinin icinde reqem YOXDUR!");
+            //else
+            //    Console.WriteLine(enterString + " stringinin icinde reqem VAR!");
+            #endregion
+
+            #region Assignment-2
+            //Console.WriteLine("String daxil edin:");
+            //string enterString = Console.ReadLine();
+            //bool hasChar = false;
+
+
+            //for(int i = 0; i < enterString.Length; i++)
+            //{
+            //    if (enterString[i] == 'a' && enterString[i + 1] == 'l')
+            //    {
+            //        hasChar = true;
+            //        break;
+            //    }
+
+            //    else
+            //        hasChar = false;
+            //}
+
+            //if (hasChar == true)
+            //    Console.WriteLine("Stringin icinde 'al' herf birlesmesi VAR!");
+
+            //else
+            //    Console.WriteLine("Stringin icinde 'al' herf birlesmesi YOXDUR!");
+            #endregion
+
+            #region Assignment-3
+            //Console.WriteLine("ededi daxil edin:");
+            //int number = Convert.ToInt32(Console.ReadLine());
+
+            //float counter = 0;
+            //float sum = 0;
+            //float result;
+
+            //while (number > 0)
+            //{
+            //    var digit = number % 10;
+            //    sum += digit;
+            //    number = (number - digit) / 10;
+            //    counter++;
+            //}
+
+            //if (counter > 0)
+            //{
+            //    result = sum / counter;
+            //    Console.WriteLine(result);
+            //}
+
+            //else
+            //    Console.WriteLine("Bu ededin reqemlerinin ceminin ededi ortasi yoxdur!");
+            #endregion
+        }
+    }
+}
