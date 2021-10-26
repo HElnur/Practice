8ca537ebb255b2b6ef66ca463a42b9c05f7dad41
Elnur Huseynov <83987253+CoderHE-999@users.noreply.github.com>
2021-10-26 14:41:39 +0400

    Update Program.cs

--- Practice/Practice/Program.cs
+++ Practice/Practice/Program.cs
@@ -7,7 +7,7 @@ namespace Practice
         static void Main(string[] args)
         {
             #region Assignment-1
-            //char[] digits = { '1', '2', '3', '4', '5', '6', '7', '7', '8', '9', '0' };
+            //char[] digits = { '1', '2', '3', '4', '5', '6', '7' , '8', '9', '0' };
             //bool hasChar = false;
 
             //Console.WriteLine("Yazi daxil edin:");
@@ -63,29 +63,34 @@ namespace Practice
             #endregion
 
             #region Assignment-3
-            //Console.WriteLine("ededi daxil edin:");
-            //int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("String daxil edin:");
+            string enterString = Console.ReadLine();
+            bool hasChar = false;
 
-            //float counter = 0;
-            //float sum = 0;
-            //float result;
 
-            //while (number > 0)
-            //{
-            //    var digit = number % 10;
-            //    sum += digit;
-            //    number = (number - digit) / 10;
-            //    counter++;
-            //}
+            for (int i = 0; i < enterString.Length; i++)
+            {
+                int k = i + 1;
 
-            //if (counter > 0)
-            //{
-            //    result = sum / counter;
-            //    Console.WriteLine(result);
-            //}
+                if (k < enterString.Length)
+                {
+                    if (enterString[i] == 'a' && enterString[i + 1] == 'l')
+                    {
+                        hasChar = true;
+                        break;
+                    }
 
-            //else
-            //    Console.WriteLine("Bu ededin reqemlerinin ceminin ededi ortasi yoxdur!");
+                    else
+                        hasChar = false;
+                }
+                
+            }
+
+            if (hasChar == true)
+                Console.WriteLine("Stringin icinde 'al' herf birlesmesi VAR!");
+
+            else
+                Console.WriteLine("Stringin icinde 'al' herf birlesmesi YOXDUR!");
             #endregion
         }
     }
